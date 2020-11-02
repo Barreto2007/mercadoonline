6e873a4f18e021fc2c63abf0fda7f99a6d9a59f6
Cleber Pinto Barreto <cleber.barreto@imetame.com.br>
2020-11-02 01:04:23 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/inovandoti/mercadoonline/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.inovandoti.mercadoonline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:mercadoonline/models/page_manager.dart';
+import 'package:provider/provider.dart';
+
+class DrawerTile extends StatelessWidget {
+
+  const  DrawerTile({this.iconData, this.title, this.page});
+  final IconData iconData;
+  final String title;
+  final int page;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final int curPage = context.watch<PageManager>().page;
+
+    return InkWell(
+      onTap:(){
+        context.read<PageManager>().setPage(page);
+
+      },
+
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          children: <Widget> [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Icon(
+                iconData,
+                size: 32,
+                color: curPage == page ? Colors.red : Colors.green[700],
+              ),
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 26,
+                color: curPage == page ? Colors.red : Colors.green[700],
+              ),
+            )
+        ],
+
+
+
+        ),
+      ),
+    );
+  }
+}
